1a530f13ab4ccd36ae8cf1e97feae355e2596219
Devcaiodias <caiodiasmartins12@gmail.com>
2024-05-20 09:59:36 -0300

    DELETE com JOINs

--- _mysql/aula23.sql
+++ _mysql/aula23.sql
@@ -1,13 +1,13 @@
 -- Active: 1715620302392@@127.0.0.1@3306@banco_de_dados
-/* Atualizando registros com join */
-UPDATE users as u
-JOIN profiles as p 
-ON p.user_id = u.id
-SET p.bio = CONCAT(p.bio, ' Atualizado')
-WHERE u.fist_name = 'Madison';
-
+/* Apagando registros com join */
 SELECT u.fist_name, p.bio 
 FROM users as u
-JOIN profiles as p 
+LEFT JOIN profiles as p 
+ON p.user_id = u.id
+WHERE u.fist_name = 'Madison';
+
+DELETE p, u
+FROM users as u
+LEFT JOIN profiles as p 
 ON p.user_id = u.id
 WHERE u.fist_name = 'Madison';
